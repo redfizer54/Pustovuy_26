9ec26159b4f69fdb63ad0da51e7d69cb9c638138
redfizer54 <pustovui@mail.ru>
2022-12-04 00:37:39 +0300

    LR_26

---
+++ 26/Assets/Scripts/Tiles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {   if (collision.tag != "Player") 
+            return;
+        if(collision.tag != transform.tag) 
+            Debug.Log("dead");
+    }
+}
